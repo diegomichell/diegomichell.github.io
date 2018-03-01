aa82efe11bc564d4feb332d599dbbd2787000b57
Diego Michel <diego.ivan.perez.michel@gmail.com>
2018-02-28 23:05:33 -0400

    #build 3, uses crossorigin.me

--- src/services/github/github-service.js
+++ src/services/github/github-service.js
@@ -1,7 +1,7 @@
 // flow
 import { GithubRepo } from '../../models/GithubRepo';
 
-const PINNED_REPOS_MICRO_SERVICE_ENDPOINT = 'https://cors.now.sh/https://ls-vpnnynhzrz.now.sh/';
+const PINNED_REPOS_MICRO_SERVICE_ENDPOINT = 'https://crossorigin.me/https://ls-vpnnynhzrz.now.sh/';
 
 export async function getPinnedRepos(username): Promise<GithubRepo> {
   const result = await fetch(PINNED_REPOS_MICRO_SERVICE_ENDPOINT + `?username=${username}`);
